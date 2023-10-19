3ff603a6d58583fb4f481561da8e80790586ffc6
TheRealFury <76801758+TheRealFury@users.noreply.github.com>
2023-10-19 12:24:12 +0530

    Simplified code by using list to set to list method for eliminating duplicates instead of manually sorting and eliminating elements from a list.

--- GetNthLowest.py
+++ GetNthLowest.py
@@ -1,24 +1,14 @@
 def getNthLowest(nums, level):
     # Levellist (type: list) will be used to arrange all numbers (integers/ floats) in ascending order. It will not have any repeated elements.
     # x (type: integer) will be used in a while loop later on in the code to cycle through a list.
-    levellist, x = [""], 0
+    levellist = [""]
     
     # The initial list is arranged in ascending order using the .sort() inbuilt function in Python.
     nums.sort()
     
-    # This while loop cycles through the list nums indirectly. The reason for not using a for loop here is because using the while loop, we can skip ahead in the loop so as to add all unique elements of nums to levellist.
-    while x < len(nums):
-        
-        #Checks if the current element occurs more than once, if so then skip some indices to go to the next unique element.
-        if nums.count(nums[x]) > 1:
-            x += nums.count(nums[x]) - 1
-            
-        # Add element on index x to the list levellist
-        levellist.append(nums[x])
-        
-        # Increment index by one.
-        x += 1
-        
+    # Remove duplicates by simply converting list to set and back to list again, and then append nums to levellist.
+    levellist.extend(list(set(nums)))
+    
     # check which level is being asked for, and repond accordingly.
     if level >= len(levellist) or level < 0:
         
